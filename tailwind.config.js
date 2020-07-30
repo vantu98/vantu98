92192536499908344d3cc55b5e3f511a464392fd
tunv <nguyenvantu191298@gmail.com>
2020-07-30 20:53:43 +0700

    add about me

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1280px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "2560px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
